9c52bc7305e876e3391801acdb8e1b6d170a0849
emiri0806 <6051_2024_06523@g.miyazaki-c.ed.jp>
2025-07-17 12:24:01 +0900

    セレクションソートを追加

---
+++ selection.py
@@ -0,0 +1,16 @@
+Kion = [0 ,11, 19, 25, 3, 14]
+n = len(Kion) - 1
+print(n)
+
+for g in range(1, n-1, 1):
+    Hoz = g
+    for j in range(g+1, n+1, 1):
+        if Kion[j] < Kion[Hoz]:
+            Hoz = j
+    if Hoz != g:
+        Kion[0] = Kion[g]
+        Kion[g] = Kion[Hoz]
+        Kion[Hoz] = Kion[0]
+
+for k in range(1, n+1, 1):
+    print(Kion[k])
